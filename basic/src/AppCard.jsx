f2c59bce0761cf8988ec884e731278c73efb6b8c
Dongja <jdh981118@naver.com>
2023-05-04 23:54:27 +0900

    deep: wrapping component

---
+++ basic/src/AppCard.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+
+export default function AppCard() {
+	return (
+		<div>
+			<Card>
+				<p>Card1</p>
+			</Card>
+
+			<Card>
+				<h1>Card2</h1>
+				<p>content</p>
+			</Card>
+
+			<Card>
+				<article>article</article>
+			</Card>
+		</div>
+	);
+}
+
+function Card({ children }) {
+	return (
+		<div
+			style={{
+				backgroundColor: 'black',
+				borderRadius: '20px',
+				color: 'white',
+				minHeight: '200px',
+				minWidth: '200px',
+				margin: '1rem',
+				padding: '1rem',
+				textAlign: 'center',
+			}}>
+			{children}
+		</div>
+	);
+}
